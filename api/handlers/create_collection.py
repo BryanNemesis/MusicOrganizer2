cc5014cdba725b86585c0ffe09125e19f6468332
Igor Binkowski <ibinkowski@apptension.com>
2021-10-28 13:17:55 +0200

    reworked create_collection

--- api/handlers/create_collection.py
+++ api/handlers/create_collection.py
@@ -5,22 +5,32 @@ from utils.utils import get_cookie_value
 
 
 def handler(event, context):
-    body = json.loads(event['body'])
-    name = body['name']
+    try:
+        name = json.loads(event["body"])["name"]
+    except (KeyError, TypeError):
+        return {
+            "statusCode": 400,
+            "body": json.dumps(
+                {"msg": f"Collection name not found in request payload."}
+            ),
+        }
+
     collection = Collection(name)
     result = collection.save_to_db()
-
-    current_user_id = get_cookie_value(event, 'user_id')
-    print(event)
+    current_user_id = get_cookie_value(event, "user_id")
     collection.add_for_user(current_user_id)
 
-    if result['ResponseMetadata']['HTTPStatusCode'] == 200:
+    if result["ResponseMetadata"]["HTTPStatusCode"] == 200:
         return {
             "statusCode": 201,
-            "body": json.dumps({"msg": f"Collection {name} with id {collection.id} created."})
+            "body": json.dumps(
+                {"msg": f"Collection {name} with id {collection.id} created."}
+            ),
         }
     else:
         return {
             "statusCode": 500,
-            "body": json.dumps({"msg": f"Problem occured when trying to save collection {name}."})
+            "body": json.dumps(
+                {"msg": f"Problem occured when trying to save collection {name}."}
+            ),
         }
