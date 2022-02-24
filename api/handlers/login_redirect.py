50aac5df3acbad8c7c6b8098c6c64d124f213f67
Igor Binkowski <ibinkowski@apptension.com>
2022-02-24 14:17:38 +0100

    Fixed login_redirect to work without the cookie set

--- api/handlers/login_redirect.py
+++ api/handlers/login_redirect.py
@@ -5,20 +5,11 @@ from utils.utils import get_cookie_value
 from models.user import User
 
 
-def handler(event, context):
-    user_id = get_cookie_value(event, "user_id")
-    if user_id:
-        user = User.get_from_db(user_id)
-        if not user:
-            body = {
-                'user_exists': False,
-                'login_url': sp.get_auth_url()
-            }
-        else:
-            body = {
-                'user_exists': True,
-            }
+def handler(event, _):
+    if user_id := get_cookie_value(event, "user_id"):
+        if User.get_from_db(user_id):
+            return {"statusCode": 200, "body": json.dumps({"user_exists": True})}
     return {
         "statusCode": 200,
-        "body": json.dumps(body)
+        "body": json.dumps({"user_exists": False, "login_url": sp.get_auth_url()}),
     }
